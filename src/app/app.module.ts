be6ac5d86d0f969bdd0c3b008fe5d46b37833e1a
Girum Gugsa <girumg@gmail.com>
2017-03-31 22:32:13 -0400

    Upgraded to Angular 4.0.1; Added Angular Material and demos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
@@ -14,7 +18,9 @@ import { FirstComponentComponent } from './first-component/first-component.compo
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
